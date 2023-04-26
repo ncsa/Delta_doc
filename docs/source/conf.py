308b6e348635ef93db9c727e4a7d2caacf6cc4cf
Craig P Steffen <craigsteffen@gmail.com>
2023-04-26 15:50:01 -0400

    Delta main page updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Delta User Documentation'
+copyright = '2023, University of Illinois'
+author = 'NCSA'
 
 release = '0.1'
 version = '0.1.0'
