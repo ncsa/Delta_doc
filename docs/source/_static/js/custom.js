46314691e3fe6e12b974ff05b3bbd7a403df2bc9
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2024-03-21 14:02:48 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -8,10 +8,10 @@ $( document ).ready(function() {
 
   var separator = document.createTextNode(" | ");
 
-  // These items are right-aligned in the RTD theme breadcrumbs
+  // This replaces the GitHub link in the upper-right with the "Documentation Hub" link
   aside = document.querySelector("body > div > section > div > div > div:nth-child(1) > ul > li.wy-breadcrumbs-aside > a");
 
-  // These items are right-aligned in the RTD theme breadcrumbs
+  // This allows you to replace the 'home' logo with the aside.repalceWith() text
   //aside = document.querySelector("body > div > section > div > div > div:nth-child(1) > ul > li");
   
   // Next to the default "Edit on GitHub", add a separator, then the hub link.
@@ -19,12 +19,12 @@ $( document ).ready(function() {
 
 
   // Insert "Documentation Hub" below html_logo in sidebar navigation
-  var docshub_link = document.createElement("a");
-  var docshub_link_text = document.createTextNode("Documentation Hub");
-  docshub_link.appendChild(docshub_link_text);
-  docshub_link.setAttribute("href","https://docs.ncsa.illinois.edu");
+  // var docshub_link = document.createElement("a");
+  // var docshub_link_text = document.createTextNode("Documentation Hub");
+  // docshub_link.appendChild(docshub_link_text);
+  // docshub_link.setAttribute("href","https://docs.ncsa.illinois.edu");
   
-  wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
-  wysidenavsearch.appendChild(docshub_link);
+  // wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
+  // wysidenavsearch.appendChild(docshub_link);
   
 });
